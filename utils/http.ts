df216c2bd7ddde120bd686d2862ff2f73a4aafa8
Drew Cook <drewcook29@gmail.com>
2022-02-17 13:52:16 -0700

    chore: Use relative routes

--- utils/http.ts
+++ utils/http.ts
@@ -1,11 +1,19 @@
 import axios from 'axios'
 
+// Create Axios instance
 const host = process.env.CLIENT_HOST
+const instance = axios.create({ baseURL: host });
 
+/**
+ * A generic GET request wrapper to ease use within React
+ * @param pathname - The name of the API route
+ * @param params - Any params to be passed in
+ * @returns - Resources from our database
+ */
 export const get = async (pathname: string, params?: any): Promise<any> => {
   console.log({host})
 	try {
-		const { data } = await axios.get(`/api${pathname}`, params)
+		const { data } = await instance.get(`/api${pathname}`, params)
 		return data
 	} catch (e: any) {
     console.error(e)
@@ -13,10 +21,16 @@ export const get = async (pathname: string, params?: any): Promise<any> => {
 	}
 }
 
+/**
+ * A generic POST request wrapper to ease use within React
+ * @param pathname - The name of the API route
+ * @param data - Any data to be passed in
+ * @returns - Resources from our database
+ */
 export const post = async (pathname: string, data?: any): Promise<any> => {
   console.log({host})
 	try {
-		const res = await axios.post(`/api${pathname}`, data)
+		const res = await instance.post(`/api${pathname}`, data)
 		return res.data
 	} catch (e: any) {
     console.error(e)
