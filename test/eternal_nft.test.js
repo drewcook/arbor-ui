18235a862f1e90c99a548d839ace5b33d93f31b4
Drew Cook <drewcook29@gmail.com>
2022-02-17 18:17:59 -0700

    feat: Add smart contracts

---
+++ test/eternal_nft.test.js
@@ -0,0 +1,65 @@
+const NFTContract = artifacts.require('EternalNFT')
+
+contract('NFTContract', accounts => {
+	let nft
+	let tokenId
+
+	// Tests it can be deployed
+	it('has been deployed', async () => {
+		const contract = await NFTContract.deployed()
+		assert(contract, 'NFT contract was not deployed')
+		console.log({ contract })
+		assert.notEqual(contract, undefined)
+	})
+
+	// // Tests address for the EternalNFT contract
+	// it('Should have an address', async () => {
+	// 	const contract = await NFTContract.deployed()
+	// 	const address = await contract.address()
+	// 	assert.notEqual(address, 0x0)
+	// 	assert.notEqual(address, '')
+	// 	assert.notEqual(address, null)
+	// 	assert.notEqual(address, undefined)
+	// })
+
+	// // Tests name for the token of EternalNFT contract
+	// it('Should have a name', async () => {
+	// 	// Returns the name of the token
+	// 	const name = await nft.collectionName()
+
+	// 	assert.equal(name, 'EternalNFT')
+	// })
+
+	// // Tests symbol for the token of EternalNFT contract
+	// it('Should have a symbol', async () => {
+	// 	// Returns the symbol of the token
+	// 	const symbol = await nft.collectionSymbol()
+
+	// 	assert.equal(symbol, 'ENFT')
+	// })
+
+	// // Tests for NFT minting function of EternalNFT contract using tokenID of the minted NFT
+	// it('Should be able to mint NFT', async () => {
+	// 	// Mints a NFT
+	// 	let txn = await nft.createEternalNFT()
+	// 	let tx = await txn.wait()
+
+	// 	// tokenID of the minted NFT
+	// 	let event = tx.events[0]
+	// 	let value = event.args[2]
+	// 	tokenId = value.toNumber()
+
+	// 	assert.equal(tokenId, 0)
+
+	// 	// Mints another NFT
+	// 	txn = await nft.createEternalNFT()
+	// 	tx = await txn.wait()
+
+	// 	// tokenID of the minted NFT
+	// 	event = tx.events[0]
+	// 	value = event.args[2]
+	// 	tokenId = value.toNumber()
+
+	// 	assert.equal(tokenId, 1)
+	// })
+})
---
+++ truffle-config.js
@@ -0,0 +1,50 @@
+// Truffle config <http://truffleframework.com/docs/advanced/configuration>
+
+const path = require('path')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+
+const networkIds = {
+	mainnet: 1,
+	ropsten: 3,
+	rinkeby: 4,
+	goerli: 5,
+	kovan: 42,
+}
+
+const getInfuraNetworkConfig = networkName => {
+	return {
+		provider: () =>
+			new HDWalletProvider({
+				mnemonic: process.env.MNEMONIC,
+				providerOrUrl: `https://${networkName}.infura.io/v3/${process.env.INFURA_KEY}`,
+			}),
+		network_id: networkIds[networkName],
+	}
+}
+
+module.exports = {
+	compilers: {
+		solc: {
+			version: '0.8.1',
+		},
+	},
+	// Contracts
+	contracts_build_directory: path.join(__dirname, 'client/contracts'),
+	// Networks
+	networks: {
+		// Local
+		develop: {
+			host: '127.0.0.1',
+			port: 7545,
+			network_id: 5777,
+		},
+		// Mainnet
+		mainnet: getInfuraNetworkConfig('mainnet'),
+		// Testnets
+		ropsten: getInfuraNetworkConfig('ropsten'),
+		rinkeby: getInfuraNetworkConfig('rinkeby'),
+		goerli: getInfuraNetworkConfig('goerli'),
+		kovan: getInfuraNetworkConfig('kovan'),
+	},
+}
