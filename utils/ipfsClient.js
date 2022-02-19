5355bd5065cc8f058f7d17238d241d1ed752b65d
Drew Cook <drewcook29@gmail.com>
2022-02-18 20:25:55 -0700

    chore: Update IPFS infura

--- utils/ipfsClient.js
+++ utils/ipfsClient.js
@@ -1,26 +1,27 @@
 import { create } from 'ipfs-http-client'
 
-// Decode token for Infura
-const b64 = 'MjRjUnBlVFYzT2Q1N1ptTzNLQmZQdUt5NVhKOjExNGUxNzhhZjUyYTg5YWVhZjU2NzY1YmVhYzhlYzhl'
-const bearerToken = 'Basic ' + Buffer.from(b64).toString()
-
 // Development (Local)
 const localNode = '/ip4/127.0.0.1/tcp/5001'
 
+// Production (NFT Storage)
+// import { NFTStorage } from 'nft.storage'
+// const nftStorageNode = new NFTStorage({ token: process.env.NFT_STORAGE_KEY })
+
 // Production (Infura)
+// Decode token for Infura
+// const b64 = 'MjRjUnBlVFYzT2Q1N1ptTzNLQmZQdUt5NVhKOjExNGUxNzhhZjUyYTg5YWVhZjU2NzY1YmVhYzhlYzhl'
+// const bearerToken = 'Basic ' + Buffer.from(b64).toString()
 const infuraNode = {
 	host: 'ipfs.infura.io',
 	port: 5001,
 	protocol: 'https',
 	headers: {
-		authorization: bearerToken,
+		authorization:
+			'Basic MjRjUnBlVFYzT2Q1N1ptTzNLQmZQdUt5NVhKOjExNGUxNzhhZjUyYTg5YWVhZjU2NzY1YmVhYzhlYzhl',
 	},
-	// apiPath: '/ipfs/api/v0',
-	apiPath: '/api/v0',
 }
 
 const ipfsClientConfig = process.env.NODE_ENV === 'production' ? infuraNode : localNode
 const ipfsClient = create(ipfsClientConfig)
-// const ipfsClient: IPFSHTTPClient = create(infuraNode) // Use infura
 
 export default ipfsClient
