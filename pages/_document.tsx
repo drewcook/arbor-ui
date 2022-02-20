9f2b18580ea34547a193c620ee6a4ba868fd31b8
Drew Cook <drewcook29@gmail.com>
2022-02-20 14:28:07 -0700

    chore: Add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,18 @@ class AppDocument extends Document {
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
 					<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-NE0CW8ZW1B"></script>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-NE0CW8ZW1B');
+              `,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
