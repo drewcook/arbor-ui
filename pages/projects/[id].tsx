7b589600ea8530a8eb144deb9e75da2313df2bc7
Drew Cook <drewcook29@gmail.com>
2022-02-20 05:36:40 -0700

    chore: Use string

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -206,7 +206,7 @@ const ProjectPage: NextPage<ProjectPageProps> = props => {
         const data = await response.json()
         if (data.success) {
           // Call smart contract and mint an nft out of the original CID
-          const sampleURI: string = await contract.methods.mint(accounts[0], data.cid, details.collaborators).send({ from: accounts[0], value: web3.utils.fromWei(10000000000000000) })
+          const sampleURI: string = await contract.methods.mint(accounts[0], data.cid, details.collaborators).send({ from: accounts[0], value: web3.utils.fromWei('10000000000000000') })
           console.log('token minted, URI:', sampleURI)
           // TODO: Do stuff?
           setSuccessOpen(true)
