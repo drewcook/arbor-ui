6a76a9bbf1e32804d43c30aec64d7bd5acb7506d
Drew Cook <drewcook29@gmail.com>
2022-03-24 17:36:39 -0600

    chore: Remove CLIENT_HOST

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
 	reactStrictMode: true,
 	env: {
 		MONGODB_URI: process.env.MONGODB_URI,
-		CLIENT_HOST: process.env.CLIENT_HOST,
 		BLOCKNATIVE_KEY: process.env.BLOCKNATIVE_KEY,
 		NFT_STORAGE_KEY: process.env.NFT_STORAGE_KEY,
 		COVALENT_API_KEY: process.env.COVALENT_API_KEY,
