c77a67041bd9c359b03f4867f5c05ba1fc795c04
Drew Cook <drewcook29@gmail.com>
2022-04-24 22:07:20 -0600

    chore: Add deployment tests

--- web3/test/PolyEchoNFT.test.js
+++ web3/test/PolyEchoNFT.test.js
@@ -1,64 +1,67 @@
 const NFTContract = artifacts.require('PolyEchoNFT')
 
-contract('PolyEchoNFT', accounts => {
+contract('PolyEchoNFT: deployment', () => {
+	let contract
 	let nft
 	let tokenId
 
+	beforeEach(async () => {
+		contract = await NFTContract.deployed()
+	})
+
 	// Tests it can be deployed
 	it('has been deployed', async () => {
-		const contract = await NFTContract.deployed()
 		assert(contract, 'PolyEchoNFT contract was not deployed')
 		assert.notEqual(contract, undefined)
 	})
 
-	// // Tests address for the EternalNFT contract
-	// it('Should have an address', async () => {
-	// 	const contract = await NFTContract.deployed()
-	// 	const address = await contract.address()
-	// 	assert.notEqual(address, 0x0)
-	// 	assert.notEqual(address, '')
-	// 	assert.notEqual(address, null)
-	// 	assert.notEqual(address, undefined)
-	// })
+	// Tests address for the PolyEchoNFT contract
+	it('Should have an address', () => {
+		const address = contract.address
+		assert.notEqual(address, 0x0)
+		assert.notEqual(address, '')
+		assert.notEqual(address, null)
+		assert.notEqual(address, undefined)
+	})
 
-	// // Tests name for the token of EternalNFT contract
-	// it('Should have a name', async () => {
-	// 	// Returns the name of the token
-	// 	const name = await nft.collectionName()
+	// Tests name for the token of PolyEchoNFT contract
+	it('Should have a name', async () => {
+		// Returns the name of the token
+		const name = await contract.name()
+		assert.equal(name, 'PolyEcho')
+	})
 
-	// 	assert.equal(name, 'EternalNFT')
-	// })
+	// Tests symbol for the token of PolyEchoNFT contract
+	it('Should have a symbol', async () => {
+		// Returns the symbol of the token
+		const symbol = await contract.symbol()
+		assert.equal(symbol, 'ECHO')
+	})
+})
 
-	// // Tests symbol for the token of EternalNFT contract
-	// it('Should have a symbol', async () => {
-	// 	// Returns the symbol of the token
-	// 	const symbol = await nft.collectionSymbol()
+contract('PolyEchoNFT: minting', () => {
+	// Tests for NFT minting function of PolyEchoNFT contract using tokenID of the minted NFT
+	it.skip('Should be able to mint NFT', async () => {
+		// Mints a NFT
+		let txn = await nft.createPolyEchoNFT()
+		let tx = await txn.wait()
 
-	// 	assert.equal(symbol, 'ENFT')
-	// })
+		// tokenID of the minted NFT
+		let event = tx.events[0]
+		let value = event.args[2]
+		tokenId = value.toNumber()
 
-	// // Tests for NFT minting function of EternalNFT contract using tokenID of the minted NFT
-	// it('Should be able to mint NFT', async () => {
-	// 	// Mints a NFT
-	// 	let txn = await nft.createEternalNFT()
-	// 	let tx = await txn.wait()
+		assert.equal(tokenId, 0)
 
-	// 	// tokenID of the minted NFT
-	// 	let event = tx.events[0]
-	// 	let value = event.args[2]
-	// 	tokenId = value.toNumber()
+		// Mints another NFT
+		txn = await nft.createPolyEchoNFT()
+		tx = await txn.wait()
 
-	// 	assert.equal(tokenId, 0)
+		// tokenID of the minted NFT
+		event = tx.events[0]
+		value = event.args[2]
+		tokenId = value.toNumber()
 
-	// 	// Mints another NFT
-	// 	txn = await nft.createEternalNFT()
-	// 	tx = await txn.wait()
-
-	// 	// tokenID of the minted NFT
-	// 	event = tx.events[0]
-	// 	value = event.args[2]
-	// 	tokenId = value.toNumber()
-
-	// 	assert.equal(tokenId, 1)
-	// })
+		assert.equal(tokenId, 1)
+	})
 })
