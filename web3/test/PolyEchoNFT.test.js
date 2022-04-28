413d0e14971cc35101e5523d5cc6151d25e091af
Drew Cook <drewcook29@gmail.com>
2022-04-28 15:39:22 -0600

    chore: Minting tests

--- web3/test/PolyEchoNFT.test.js
+++ web3/test/PolyEchoNFT.test.js
@@ -2,20 +2,16 @@ const NFTContract = artifacts.require('PolyEchoNFT')
 
 contract('PolyEchoNFT: deployment', () => {
 	let contract
-	let nft
-	let tokenId
 
 	beforeEach(async () => {
 		contract = await NFTContract.deployed()
 	})
 
-	// Tests it can be deployed
 	it('has been deployed', async () => {
 		assert(contract, 'PolyEchoNFT contract was not deployed')
 		assert.notEqual(contract, undefined)
 	})
 
-	// Tests address for the PolyEchoNFT contract
 	it('Should have an address', () => {
 		const address = contract.address
 		assert.notEqual(address, 0x0)
@@ -24,44 +20,119 @@ contract('PolyEchoNFT: deployment', () => {
 		assert.notEqual(address, undefined)
 	})
 
-	// Tests name for the token of PolyEchoNFT contract
 	it('Should have a name', async () => {
-		// Returns the name of the token
 		const name = await contract.name()
 		assert.equal(name, 'PolyEcho')
 	})
 
-	// Tests symbol for the token of PolyEchoNFT contract
 	it('Should have a symbol', async () => {
-		// Returns the symbol of the token
 		const symbol = await contract.symbol()
 		assert.equal(symbol, 'ECHO')
 	})
 })
 
-contract('PolyEchoNFT: minting', () => {
+contract('PolyEchoNFT: properties', accounts => {
+	let contract
+	const owner = accounts[0]
+
+	beforeEach(async () => {
+		contract = await NFTContract.deployed()
+	})
+
+	it('Should have a display name', async () => {
+		const displayName = await contract.displayName()
+		assert.equal(displayName, 'PolyEcho Audio NFT')
+	})
+
+	it('Should have a company name', async () => {
+		const companyName = await contract.companyName()
+		assert.equal(companyName, 'PolyEcho')
+	})
+
+	describe('editing displayName', () => {
+		it('Should be able to update the display name', async () => {
+			const displayName = await contract.displayName()
+			assert.equal(displayName, 'PolyEcho Audio NFT')
+		})
+
+		it('should emit the NameUpdated event', async () => {
+			const tx = await contract.updateDisplayName('Genesis Collection', {
+				from: owner,
+			})
+			const expectedEvent = 'NameUpdated'
+			const actualEvent = tx.logs[0].event
+			assert.equal(actualEvent, expectedEvent, 'events should match')
+		})
+	})
+})
+
+contract('PolyEchoNFT: mintAndBuy()', accounts => {
+	let contract
+	const owner = accounts[0]
+	const minter = accounts[1]
+	const metadataURI1 = 'ipfs://some-test-CID-1'
+	const metadataURI2 = 'ipfs://some-test-CID-2'
+	const contributors = [accounts[2], accounts[3], accounts[3]]
+
+	beforeEach(async () => {
+		contract = await NFTContract.deployed()
+	})
+
+	it('Should have a static mint price', async () => {
+		const mintPrice = await contract.mintPrice()
+		assert.equal(mintPrice, 10000000000000000)
+	})
+
+	it('Throws an error if sender sends less than the mint price', async () => {
+		try {
+			await contract.mintAndBuy(minter, metadataURI1, contributors)
+		} catch (err) {
+			const expectedError = 'Sent ether value is not enough to mint'
+			const actualError = err.reason
+			assert.equal(actualError, expectedError, 'should not be permitted')
+		}
+	})
+
 	// Tests for NFT minting function of PolyEchoNFT contract using tokenID of the minted NFT
-	it.skip('Should be able to mint NFT', async () => {
-		// Mints a NFT
-		let txn = await nft.createPolyEchoNFT()
-		let tx = await txn.wait()
+	it('Should be able to mint NFTs with proper metadata', async () => {
+		// Mint a token
+		const mintPrice = await contract.mintPrice()
+		let tx = await contract.mintAndBuy(minter, metadataURI1, contributors, {
+			value: mintPrice,
+		})
 
-		// tokenID of the minted NFT
-		let event = tx.events[0]
-		let value = event.args[2]
-		tokenId = value.toNumber()
+		// Test tokenID and URI
+		let tokenId = tx.logs[0].args[2].toNumber()
+		let actualMetadataURI = tx.logs[2].args._tokenURI
+		assert.equal(tokenId, 1, 'tokenID should start with 1')
+		assert.equal(actualMetadataURI, metadataURI1, 'metadata URIs should match')
 
-		assert.equal(tokenId, 0)
+		// Emits the custom TokenCreated event
+		const expectedEvent = 'TokenCreated'
+		let actualEvent = tx.logs[2].event
+		assert.equal(actualEvent, expectedEvent, 'events should match')
 
-		// Mints another NFT
-		txn = await nft.createPolyEchoNFT()
-		tx = await txn.wait()
+		// Mint another token and test incremented tokenID
+		tx = await contract.mintAndBuy(minter, metadataURI2, contributors, {
+			value: mintPrice,
+		})
+		tokenId = tx.logs[0].args[2].toNumber()
+		actualMetadataURI = tx.logs[2].args._tokenURI
+		assert.equal(tokenId, 2, 'tokenID should be incremented')
+		assert.equal(actualMetadataURI, metadataURI2, 'metadata URIs should match')
+		const tokenIdURIState = await contract.tokenIdToUri()[tokenId - 1]
+		assert.equal(tokenIdURIState, actualMetadataURI, 'state should be equal')
+	})
 
-		// tokenID of the minted NFT
-		event = tx.events[0]
-		value = event.args[2]
-		tokenId = value.toNumber()
-
-		assert.equal(tokenId, 1)
+	// TODO:
+	it.skip('Should pay out the mint price evenly to contributors', async () => {
+		const mintPrice = await contract.mintPrice()
+		const expectedContributorCut = mintPrice / contributors.length
+		const tx = await contract.mintAndBuy(minter, metadataURI1, contributors, {
+			value: mintPrice,
+		})
+		// It is possible that we should emit an event for each transfer to a contributor, and test that way
+		const actualContributors = await contract.tokenIdToContributors(0)
+		assert.equal(actualContributors, contributors, 'contributors should be tied to the newly minted tokenID')
 	})
 })
