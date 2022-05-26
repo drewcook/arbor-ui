991d6160c8be0d6873f4e843ac7b9a6d1ff216ca
Drew Cook <drewcook29@gmail.com>
2022-05-26 14:08:10 -0600

    chore: Test downloading

--- utils/downloadURL.ts
+++ utils/downloadURL.ts
@@ -12,52 +12,48 @@ import logger from './logger'
  */
 const downloadURL = async (url: string, downloadDir: string, downloadPath: string): Promise<string> => {
 	// TODO: Figure out how to check if file exists already in directory and re-use, fs.access is synchronous
-
-	// Delete directory and start fresh each time if exists
 	try {
+		// Delete directory and start fresh each time if exists
 		if (fs.existsSync(downloadDir)) fs.rmdirSync(downloadDir, { recursive: true })
-		console.log(__dirname, downloadDir)
-		return Promise.resolve('')
+		fs.mkdirSync(downloadDir, { recursive: true })
+		// Create write stream
+		const writer = fs.createWriteStream(downloadPath)
 
+		// Create read stream with axios
+		const response = await axios({
+			url,
+			method: 'GET',
+			responseType: 'stream',
+		})
+
+		// Pipe read stream into the write stream
+		response.data.pipe(writer)
+
+		// Resolve when finished streaming, reject if any error
+		return new Promise((resolve, reject) => {
+			writer.on('finish', () => {
+				return resolve(downloadPath)
+			})
+			writer.on('error', reject)
+		})
+
+		// ====== FOR REFERENCE ONLY ======
+		// Alternatively, Use NFT.storage API to download
+		// const nftStorage = axios.create({
+		// 	baseURL: 'https://api.nft.storage',
+		// 	responseType: 'arraybuffer',
+		// 	headers: {
+		// 		authorization: `Bearer ${process.env.NFT_STORAGE_KEY}`,
+		// 	},
+		// })
+		// cid = cid.replace('ipfs://', '').split('/')[0]
+		// console.log({ cid })
+		// const response = await nftStorage.get(`/${cid}`)
+		// const buffer = Buffer.from(response.data, 'utf-8') // same as response.data
 	} catch (e: any) {
 		console.error('Error downloading URL', e)
 		return Promise.reject(e)
 	}
-	// Create write stream
-	// fs.mkdirSync(downloadDir, { recursive: true })
-	// const writer = fs.createWriteStream(downloadPath)
-
-	// // Create read stream with axios
-	// const response = await axios({
-	// 	url,
-	// 	method: 'GET',
-	// 	responseType: 'stream',
-	// })
-
-	// // Pipe read stream into the write stream
-	// response.data.pipe(writer)
-
-	// // Resolve when finished streaming, reject if any error
-	// return new Promise((resolve, reject) => {
-	// 	writer.on('finish', () => {
-	// 		return resolve(downloadPath)
-	// 	})
-	// 	writer.on('error', reject)
-	// })
-
-	// ====== FOR REFERENCE ONLY ======
-	// Alternatively, Use NFT.storage API to download
-	// const nftStorage = axios.create({
-	// 	baseURL: 'https://api.nft.storage',
-	// 	responseType: 'arraybuffer',
-	// 	headers: {
-	// 		authorization: `Bearer ${process.env.NFT_STORAGE_KEY}`,
-	// 	},
-	// })
-	// cid = cid.replace('ipfs://', '').split('/')[0]
-	// console.log({ cid })
-	// const response = await nftStorage.get(`/${cid}`)
-	// const buffer = Buffer.from(response.data, 'utf-8') // same as response.data
 }
 
 /**
@@ -67,50 +63,48 @@ const downloadURL = async (url: string, downloadDir: string, downloadPath: strin
  * @returns {Promise}
  */
 export const zipDirectory = (sourceDir: string, outDir: string, filename: string): Promise<string> => {
-	// Create archiver to compress with zlib
-	// A higher level will result in better compression, but will take longer to complete. A lower level will result in less compression, but will be much faster. Level 5 is a good balance.s
-	const archive = archiver('zip', { zlib: { level: 5 } })
-	// Delete directory and start fresh each time if exists
 	try {
+		// Create archiver to compress with zlib
+		// A higher level will result in better compression, but will take longer to complete. A lower level will result in less compression, but will be much faster. Level 5 is a good balance.s
+		const archive = archiver('zip', { zlib: { level: 5 } })
+		// Delete directory and start fresh each time if exists
 		if (fs.existsSync(outDir)) fs.rmdirSync(outDir, { recursive: true })
-		// fs.mkdirSync(outDir, { recursive: true })
-		logger.blue(__dirname)
-		logger.yellow(sourceDir)
-		logger.magenta(outDir)
-		return Promise.resolve('')
+		fs.mkdirSync(outDir, { recursive: true })
+
+		// Create write stream
+		const writer = fs.createWriteStream(`${outDir}/${filename}`)
+
+		return new Promise((resolve, reject) => {
+			// Use Archiver to compress directory and pipe it into the write stream
+			archive
+				.directory(sourceDir, false)
+				// .on('progress', ({ entries, fs }) => console.log('progress', { entries, fs }))
+				.on('warning', err => {
+					if (err.code === 'ENOENT') {
+						// log warning
+						console.warn(`ARCHIVER WARNING - ${err.message}`)
+						console.warn(err.data)
+					} else {
+						// throw error
+						throw err
+					}
+				})
+				.on('error', reject)
+				.pipe(writer)
+			// Listen to writer stream events
+			writer.on('finish', () => resolve(`${outDir}/${filename}`))
+			writer.on('error', err => {
+				console.error('ARCHIVER ERROR', err)
+				reject(err)
+			})
+			writer.on('close', () => console.log(`File downloaded. Total of ${archive.pointer()} bytes transferred.`))
+			// https://www.archiverjs.com/docs/archiver/#finalize
+			archive.finalize()
+		})
 	} catch (e: any) {
 		console.error(e)
+		return Promise.reject(e)
 	}
-	// Create write stream
-	const writer = fs.createWriteStream(`${outDir}/${filename}`)
-
-	return new Promise((resolve, reject) => {
-		// Use Archiver to compress directory and pipe it into the write stream
-		archive
-			.directory(sourceDir, false)
-			// .on('progress', ({ entries, fs }) => console.log('progress', { entries, fs }))
-			.on('warning', err => {
-				if (err.code === 'ENOENT') {
-					// log warning
-					console.warn(`ARCHIVER WARNING - ${err.message}`)
-					console.warn(err.data)
-				} else {
-					// throw error
-					throw err
-				}
-			})
-			.on('error', reject)
-			.pipe(writer)
-		// Listen to writer stream events
-		writer.on('finish', () => resolve(`${outDir}/${filename}`))
-		writer.on('error', err => {
-			console.error('ARCHIVER ERROR', err)
-			reject(err)
-		})
-		writer.on('close', () => console.log(`File downloaded. Total of ${archive.pointer()} bytes transferred.`))
-		// https://www.archiverjs.com/docs/archiver/#finalize
-		archive.finalize()
-	})
 }
 
 export default downloadURL
