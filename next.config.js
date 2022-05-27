eaa4af47c084b1943a65a89cd1415fcf4e38f640
Drew Cook <drewcook29@gmail.com>
2022-05-27 00:37:33 -0600

    chore: Add withSentryConfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
@@ -25,4 +27,15 @@ const nextConfig = {
 	},
 }
 
-module.exports = nextConfig
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(nextConfig, {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+})
+}
