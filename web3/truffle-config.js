45c5a518bc9439a9d1bc6cad2ab4457ba4a41805
Drew Cook <drewcook29@gmail.com>
2022-06-07 22:25:12 -0600

    chore: Re-deploy to testnet

--- web3/truffle-config.js
+++ web3/truffle-config.js
@@ -1,7 +1,8 @@
- // Truffle config <http://truffleframework.com/docs/advanced/configuration>
+// Truffle config <http://truffleframework.com/docs/advanced/configuration>
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 require('dotenv').config()
 /* eslint-enable @typescript-eslint/no-var-requires */
 
@@ -9,6 +10,13 @@ module.exports = {
 	compilers: {
 		solc: {
 			version: '0.8.1',
+			settings: {
+				// See the solidity docs for advice about optimization and evmVersion
+				optimizer: {
+					enabled: false,
+					runs: 200,
+				},
+			},
 		},
 	},
 	// Contracts
@@ -21,6 +29,25 @@ module.exports = {
 			port: 8545,
 			network_id: 1337,
 		},
-		// Kardiachain Testnet and Mainnet deployed via Remix IDE
+		// KardiaChain Mainnet and Testnet
+		// See - https://docs.kardiachain.io/docs/for-developers/tutorials/smart-contract-development/dev-environment/ide-and-tools/truffle
+		kardiachain: {
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc.kardiachain.io`),
+			network_id: 0,
+			confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+			timeoutBlocks: 2000, // # of blocks before a deployment times out, (minimum/default: 50)
+			production: true,
+		},
+		kardiachain_testnet: {
+			provider: () =>
+				new HDWalletProvider({
+					mnemonic: process.env.MNEMONIC,
+					providerOrUrl: `https://dev.kardiachain.io`,
+					pollingInterval: 1000,
+				}),
+			network_id: 69,
+			timeoutBlocks: 2000,
+			skipDryRun: true,
+		},
 	},
 }
