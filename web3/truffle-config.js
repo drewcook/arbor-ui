7ead5a00c4d9c714cbe01f9854f18e7420073ff9
Drew Cook <drewcook29@gmail.com>
2022-06-22 17:34:34 -0600

    chore: Optimize truffle deployer

--- web3/truffle-config.js
+++ web3/truffle-config.js
@@ -8,6 +8,13 @@ module.exports = {
 	compilers: {
 		solc: {
 			version: '0.8.1',
+			settings: {
+				// See the solidity docs for advice about optimization and evmVersion
+				optimizer: {
+					enabled: false,
+					runs: 200,
+				},
+			},
 		},
 	},
 	// Contracts
