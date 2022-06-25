01883053bc86fd55e349db0e6a589c9b60f87781
Drew Cook <drewcook29@gmail.com>
2022-06-25 12:17:08 -0600

    chore: Compile StemQueue

--- web3/contracts/StemQueue.sol
+++ web3/contracts/StemQueue.sol
@@ -2,13 +2,14 @@
 pragma solidity >=0.8.0;
 
 import "@semaphore-protocol/contracts/interfaces/IVerifier.sol";
+import "@semaphore-protocol/contracts/Semaphore.sol";
 import "@semaphore-protocol/contracts/base/SemaphoreCore.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@semaphore-protocol/contracts/base/SemaphoreGroups.sol";
 
 /// @title StemQueue contract.
 /// @dev The following code is just a example to show how Semaphore con be used.
 /// @dev StemQueue holds the votes around the stems within each project's stem queue
-contract StemQueue is SemaphoreCore, Ownable {
+abstract contract StemQueue is Semaphore {
     // A new vote is published every time a user's proof is validated.
     event NewVote(bytes32 vote);
 
@@ -24,6 +25,32 @@ contract StemQueue is SemaphoreCore, Ownable {
         verifier = IVerifier(_verifier);
     }
 
+		function createProjectGroup(
+        uint256 groupId,
+        uint8 depth,
+        uint256 zeroValue,
+        address admin
+    ) external  {
+        _createGroup(groupId, depth, zeroValue);
+
+        groupAdmins[groupId] = admin;
+
+        emit GroupAdminUpdated(groupId, address(0), admin);
+    }
+
+    function addMemberToProjectGroup(uint256 groupId, uint256 identityCommitment) external  {
+        _addMember(groupId, identityCommitment);
+    }
+
+    function removeMemberFromProjectGroup(
+        uint256 groupId,
+        uint256 identityCommitment,
+        uint256[] calldata proofSiblings,
+        uint8[] calldata proofPathIndices
+    ) external  onlyGroupAdmin(groupId) {
+        _removeMember(groupId, identityCommitment, proofSiblings, proofPathIndices);
+    }
+
     // Only users who create valid proofs can vote.
     // The contract owner must only send the transaction and they will not know anything about the identity of the voters.
     // The external nullifier is in this example the root of the Merkle tree.
@@ -31,7 +58,7 @@ contract StemQueue is SemaphoreCore, Ownable {
         bytes32 _vote,
         uint256 _nullifierHash,
         uint256[8] calldata _proof
-    ) external onlyOwner {
+    ) external {
         _verifyProof(_vote, voters, _nullifierHash, voters, _proof, verifier);
 
         // Prevent double-voting (nullifierHash = hash(root + identityNullifier)).
