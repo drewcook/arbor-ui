4fc2f512bf2c4ef9d70a0a3b44855bb385d09abf
Drew Cook <drewcook29@gmail.com>
2022-06-25 23:44:31 -0600

    chore: Update for preferred network local on dev

--- constants/networks.ts
+++ constants/networks.ts
@@ -1,5 +1,11 @@
 // Supported networks
 const networks = {
+	localhost: {
+		networkId: 31337, // Hardhat, otherwise 1337 for Ganache
+		networkHex: '0x7A69',
+		displayName: 'Localhost',
+		rpc: 'http://127.0.0.1:8545/',
+	},
 	harmonyDevnet: {
 		networkId: 1666900000,
 		networkHex: '0x635AE020',
@@ -8,12 +14,17 @@ const networks = {
 	},
 }
 
+// Preferred network
+// Dev: localhost
+// Prod: Harmony Devnet
+const preferredNetwork = process.env.NODE_ENV === 'development' ? 'localhost' : 'harmonyDevnet'
+
 // Onboard takes hexadecimal values
-export const NETWORK_HEX = networks.harmonyDevnet.networkHex
+export const NETWORK_HEX = networks[preferredNetwork].networkHex
 
 // Covalent takes in decimal value
-export const NETWORK_ID = networks.harmonyDevnet.networkId
+export const NETWORK_ID = networks[preferredNetwork].networkId
 
-export const NETWORK_NAME = networks.harmonyDevnet.displayName
+export const NETWORK_NAME = networks[preferredNetwork].displayName
 
-export const NETWORK_RPC = networks.harmonyDevnet.rpc
+export const NETWORK_RPC = networks[preferredNetwork].rpc
