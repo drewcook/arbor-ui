c7b07303e27aead7daba0cf176d90d867f3b7917
Drew Cook <drew@dco.dev>
2022-11-06 20:48:15 +0000

    fix: hotfix for existing user avatars

--- components/ConnectedAccount.tsx
+++ components/ConnectedAccount.tsx
@@ -40,9 +40,9 @@ const ConnectedAccount = (): JSX.Element => {
 							<Avatar
 								alt="User Avatar"
 								src={
-									!currentUser.avatar
-										? 'https://www.gravatar.com/avatar/94d093eda664addd6e450d7e9881bcad?s=32&d=identicon&r=PG'
-										: currentUser.avatar.base64
+									currentUser.avatar?.base64
+										? currentUser.avatar.base64
+										: 'https://www.gravatar.com/avatar/94d093eda664addd6e450d7e9881bcad?s=32&d=identicon&r=PG'
 								}
 							/>
 						</IconButton>
