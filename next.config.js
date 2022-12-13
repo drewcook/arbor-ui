893c3c96d2555d1ec07ec953e0f5599e6889e1db
Drew Cook <drew@dco.dev>
2022-12-13 11:14:08 -0700

    chore: add in headers to next config

--- next.config.js
+++ next.config.js
@@ -31,6 +31,26 @@ const nextConfig = {
 		],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 	},
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: '/api/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: 'https://arbor-pr-*.herokuapp.com' },
+					{ key: 'Access-Control-Allow-Origin', value: 'https://ui-*-arbor-protocol.vercel.app' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+					// {
+					// 	key: 'Access-Control-Allow-Headers',
+					// 	value:
+					// 		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					// },
+				],
+			},
+		]
+	},
+	reactStrictMode: true,
 	webpack: (config, options) => {
 		if (!options.isServer) {
 			config.resolve.fallback.fs = false
