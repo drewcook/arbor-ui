f419e3f82b4fe99d54cfda57467ae9e451fa1c24
Drew Cook <drew@dco.dev>
2022-12-16 21:34:56 -0700

    chore: bypass sentry logging

--- next.config.js
+++ next.config.js
@@ -88,6 +88,8 @@ const sentryWebpackPluginOptions = {
 	// silent: true
 }
 
+module.exports = withBundleAnalyzer(nextConfig)
+
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(withBundleAnalyzer(nextConfig), sentryWebpackPluginOptions)
+// module.exports = withSentryConfig(withBundleAnalyzer(nextConfig), sentryWebpackPluginOptions)
