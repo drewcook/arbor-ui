8fd811d8c38bd73712b062364a8c7be6fc939e09
Noah Dahlman <60643920+noahdahlman@users.noreply.github.com>
2022-12-22 20:37:10 -0800

    fix: move recent projects history on homepage (#232)
    * Reposition recent projects below banner.
    
    Centered and bordered the heading.
    
    * Hide scrollbar but keep scroll functionality.
    
    * Revert "chore: test sentry"
    
    This reverts commit 2b2b204aab7362c85186755ce354978ac310b959.
    
    * chore: move container within
    
    Co-authored-by: Drew Cook <drew@dco.dev>
    Co-authored-by: Drew Cook <drewcook29@gmail.com>

--- components/RecentProjectActivity.tsx
+++ components/RecentProjectActivity.tsx
@@ -1,5 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Container, Grid, Typography } from '@mui/material'
 
 import ProjectCard from './ProjectCard'
 import styles from './RecentProjectActivity.styles'
@@ -11,17 +10,19 @@ type RecentProjectActivityProps = {
 const RecentProjectActivity = ({ projects }: RecentProjectActivityProps): JSX.Element => {
 	return (
 		<Box sx={styles.recentActivity} component="section">
-			<Typography variant="h2" sx={styles.header}>
-				Recent Activity
-			</Typography>
-			<Typography sx={styles.subheader}>Check out a few of the most recently updated projects on Arbor.</Typography>
-			<Grid container spacing={3}>
-				{projects.map(project => (
-					<Grid item xs={12} sm={4} key={project._id}>
-						<ProjectCard details={project} />
-					</Grid>
-				))}
-			</Grid>
+			<Container maxWidth="xl">
+				<Typography variant="h2" sx={styles.header}>
+					Recent Activity
+				</Typography>
+				<Typography sx={styles.subheader}>Check out a few of the most recently updated projects on Arbor.</Typography>
+				<Grid container spacing={3}>
+					{projects.map(project => (
+						<Grid item xs={12} sm={4} key={project._id}>
+							<ProjectCard details={project} />
+						</Grid>
+					))}
+				</Grid>
+			</Container>
 		</Box>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage<HomeProps> = ({ projects }) => {
 			<Head>
 				<title>Arbor | A Generative Music NFT Platform</title>
 			</Head>
-			<Box sx={styles.banner} component="section" className="homepage-banner">
+			<Box sx={styles.banner} component="section">
 				<Container maxWidth="xl">
 					<Typography variant="h2" sx={styles.heading}>
 						Create Together,
@@ -76,19 +76,12 @@ const Home: NextPage<HomeProps> = ({ projects }) => {
 					</List>
 				</Container>
 			</Box>
+			<RecentProjectActivity projects={projects} />
 			<Box sx={styles.features} component="section">
 				<Container maxWidth="xl">
 					<Typography variant="h2" sx={styles.sectionHeading}>
 						How It Works
 					</Typography>
-					<Button
-						variant="contained"
-						onClick={() => {
-							throw new Error('Testing client-side error')
-						}}
-					>
-						See How
-					</Button>
 					<Typography sx={styles.howItWorksText}>
 						Songs are created publicly via stems and sold as NFTs. Proceeds are split equally among artists.
 					</Typography>
@@ -237,9 +230,6 @@ const Home: NextPage<HomeProps> = ({ projects }) => {
 					<Faq />
 				</Container>
 			</Box>
-			<Container maxWidth="xl">
-				<RecentProjectActivity projects={projects} />
-			</Container>
 		</>
 	)
 }
